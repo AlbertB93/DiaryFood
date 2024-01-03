e44f99452660c9199288e2181423e50025c83c29
Albert B <bercik93@interia.eu>
2024-01-03 21:40:50 +0100

    Work on Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,11 @@
+/* import styles from "../CSS/menu.module.css"; */
+
 import "../CSS/menu.css";
 
 export function Menu() {
-  return <div className="menu">Menu</div>;
+  return (
+    <div className="menu">
+      <h2>Menu</h2>
+    </div>
+  );
 }
